48d6334023b3a5fda02fe4a7e2b05ce9a000324e
chuaz <zchua@nd.edu>
2021-07-22 02:38:14 -0400

    Pulled ID from form

--- src/Components/Main/ViewSelectorMapper.js
+++ src/Components/Main/ViewSelectorMapper.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+
+const formReducer = (state, e) => {
+  return {
+    ...state,
+    [e.name]: e.value
+  }
+}
 
 const ViewSelectorMapper = ({ options }) => {
+  const [formData, setFormData] = useReducer(formReducer, {})
+  const [submitting, setSubmitting] = useState(false);
+  const handleSubmit = e => {
+    e.preventDefault();
+    setSubmitting(true);
+  }
+
+  const handleChange = e => {
+    setFormData({
+      value: e.target.value,
+    });
+  }
+
   return (
     <div>
-      <form>
-        <select defaultValue="-- select a saved game --">
-          <option disabled>-- select a saved game --</option>
-          {options.map(
-            (option) =>
-              ( <option key={option.id} value={option}>
-                {option.attributes.Title}
-                </option>)
-          )}
-        </select>
-        <br /><br />
-        <button type="submit" name="button">Submit</button>
+      {submitting &&
+        <div>
+          You requested the following game:
+          <ul>
+            {Object.entries(formData).map(([name, value]) => (
+              <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+            ))}
+          </ul>
+        </div>
+      }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <select defaultValue="-- select a saved game --" name="gameInput" onChange={handleChange}>
+            <option disabled>-- select a saved game --</option>
+            {options.map(
+              (option) =>
+                ( <option key={option.id} value={option.id}>
+                  {option.attributes.Title}
+                  </option>)
+            )}
+          </select>
+          <br /><br />
+          <button>Submit</button>
+        </fieldset>
       </form>
     </div>
   );
