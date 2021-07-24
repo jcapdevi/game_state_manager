365a06e29d3fbb059c0321edce450bc90d706623
jcapdevi <jcapdevi@nd.edu>
2021-07-23 20:26:04 -0400

    Updated Storage Format

--- src/Components/Main/Subcomponents/DisplaySelection.js
+++ src/Components/Main/Subcomponents/DisplaySelection.js
@@ -10,7 +10,11 @@ const DisplaySelection = ({ selection }) => {
         <li>Template: {selection.attributes.Template}</li>
         <li>Player X: {selection.attributes.Player_x}</li>
         <li>Player O: {selection.attributes.Player_y}</li>
-        <li>Board: {selection.attributes.board}</li>
+        <li>Board:
+          <p>{selection.attributes.A1} {selection.attributes.A2} {selection.attributes.A3}</p>
+          <p>{selection.attributes.B1} {selection.attributes.B2} {selection.attributes.B3}</p>
+          <p>{selection.attributes.C1} {selection.attributes.C2} {selection.attributes.C3}</p>
+        </li>
 
         {/* TODO: Improve Board Display Method */}
 
--- src/Components/Main/Subcomponents/GameForm.js
+++ src/Components/Main/Subcomponents/GameForm.js
@@ -7,32 +7,32 @@ const GameForm = ({ game, onChange, onSubmit }) => {
         <h1>Save Your Game!</h1>
         <hr />
         <label htmlFor="title">Title: </label>
-        <input type="text" id="title" name="Title"  value={game.Title}/>
+        <input type="text" id="title" name="Title"  value={game.Title} onChange={onChange} />
         <br /><br />
         <label htmlFor="game">Select game type: </label>
-        <select name="game" id="game" defaultValue="-- select an option --" value={game.Template} required>
-          <option disabled>-- select an option --</option>
-          <option value="ttt">Tic-Tac-Toe</option>
+        <select name="Template" id="game" defaultValue="-- select an option --" onChange={onChange} required>
+          <option disabled value="-- select an option --">-- select an option --</option>
+          <option value="tic-tac-toe">Tic-Tac-Toe</option>
         </select>
         <br /><br />
         <label htmlFor="player-x">Name of Player X: </label>
-        <input type="text" id="player-x" name="Player_x"  value={game.Player_x}/>
+        <input type="text" id="player-x" name="Player_x"  value={game.Player_x} onChange={onChange} />
         <br /><br />
         <label htmlFor="player-o">Name of Player O: </label>
-        <input type="text" id="player-o" name="Player_y"  value={game.Player_y}/>
+        <input type="text" id="player-o" name="Player_y"  value={game.Player_y} onChange={onChange} />
         <br />
         <p>Enter current game-state:</p>
-        <input type="text" id="a1" name="board[0]" maxLength="1" size="1" value={game.board[0]} />
-        <input type="text" id="a2" name="board[1]" maxLength="1" size="1" value={game.board[1]} />
-        <input type="text" id="a3" name="board[2]" maxLength="1" size="1" value={game.board[2]} />
+        <input type="text" id="a1" name="A1" maxLength="1" size="1" value={game.A1} onChange={onChange} />
+        <input type="text" id="a2" name="A2" maxLength="1" size="1" value={game.A2} onChange={onChange} />
+        <input type="text" id="a3" name="A3" maxLength="1" size="1" value={game.A3} onChange={onChange} />
         <br />
-        <input type="text" id="b1" name="board[3]" maxLength="1" size="1" value={game.board[3]} />
-        <input type="text" id="b2" name="board[4]" maxLength="1" size="1" value={game.board[4]} />
-        <input type="text" id="b3" name="board[5]" maxLength="1" size="1" value={game.board[5]} />
+        <input type="text" id="b1" name="B1" maxLength="1" size="1" value={game.B1} onChange={onChange} />
+        <input type="text" id="b2" name="B2" maxLength="1" size="1" value={game.B2} onChange={onChange} />
+        <input type="text" id="b3" name="B3" maxLength="1" size="1" value={game.B3} onChange={onChange} />
         <br />
-        <input type="text" id="c1" name="board[6]" maxLength="1" size="1" value={game.board[6]} />
-        <input type="text" id="c2" name="board[7]" maxLength="1" size="1" value={game.board[7]} />
-        <input type="text" id="c3" name="board[8]" maxLength="1" size="1" value={game.board[8]} />
+        <input type="text" id="c1" name="C1" maxLength="1" size="1" value={game.C1} onChange={onChange} />
+        <input type="text" id="c2" name="C2" maxLength="1" size="1" value={game.C2} onChange={onChange} />
+        <input type="text" id="c3" name="C3" maxLength="1" size="1" value={game.C3} onChange={onChange} />
         <br /><br />
         <button type="submit" name="button" onSubmit={onSubmit}>Submit</button>
         <br /><br />
